ea79025bfa63aedb50975b68e053fc41830d8237
Sanjeet Kotarya <sanjeetkotarya@gmail.com>
2023-04-29 09:42:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,22 +12,6 @@ async function checkWeather(city){
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "images/clouds.png";
-    }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "images/mist.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "images/drizzle.png";
-    }
-
 }
 function getSelectValue(){
     var cityname = document.querySelector(".search select").value;
